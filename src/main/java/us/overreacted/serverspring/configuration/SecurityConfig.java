715adad4bd907b6ab2ba2db92200e31f9a1c33ed
Ablax <ablax@ablax.me>
2020-12-20 23:00:06 +0200

    Add openapi

--- src/main/java/us/overreacted/serverspring/configuration/SecurityConfig.java
+++ src/main/java/us/overreacted/serverspring/configuration/SecurityConfig.java
@@ -1,9 +1,14 @@
 package us.overreacted.serverspring.configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -11,4 +16,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(final HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests().anyRequest().permitAll();
     }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Simple API")
+                        .version("1.0")
+                        .description("Simple API Description"));
+    }
+
+
 }
